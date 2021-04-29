9d8c4e7be8a2a6ec8896a66c0cbd14993c4bedb9
yahyaarakil <47091916+yahyaarakil@users.noreply.github.com>
2021-04-29 14:46:42 +0300

    General Rewrite (Finished)

--- Lab2/atmel-files/AssemblerApplication2/main.asm
+++ Lab2/atmel-files/AssemblerApplication2/main.asm
@@ -1,33 +1,275 @@
+.include "m128def.inc"
 
 .equ CRC_POL = 0x35 ;110101 ; x^5 + x^4 + 0*x^3 + x^2 + x^1 1
-; Replace with your application code
 
+.equ NEWRAMEND = 0x10FF
+
+.def PACKET_IN_REGISTER = R21
+
+; MAIN BRANCH
 .org 0x00
-jmp start
+; program starts here linearly
+CONF:
+	; constant registers
+	ldi r17, 0x00	; zero register
+	ldi r18, 0xFF	; one register
+	mov r1, r17
+	mov r2, r18
+	
+	; configure ports
+	ldi r16, 0x08
+	sts ddrf, r16	; user input and sensor interaction
+	out ddrb, r2	; ram dump output
+	out ddrd, r1	; command input
+	out ddre, r2	; command output
 
-CONF_PORTS:
-	push r17 ; store r17
-	push r18 ; r18 so we can manipulate
-	ldi r17, 0x00	; constant zero register
-	ldi r18, 0xFF	; constant one register
-	sts ddrf, r17	; user input
-	out ddrb, r17	; ram dump input ???
-	out ddrd, r17	; command input
-	out ddre, r18	; command output
-	pop r18	; retreive r18
-	pop r17	; retreive r17
-	ret
+	; configure xmem mode
+    ldi r16, 0x80
+    out mcucr, r16
 
-INIT:	
-	; Initialize stack pointer
-	push r16
-	ldi r16,low(RAMEND)
-	out spl,r16
-	ldi r16,high(RAMEND)
-	out sph,r16
-	pop r16
+	; Y will be used as memory pointer
+	ldi yl, 0x00
+	ldi yh, 0x01
+	
+
+INIT: ; Transmits a Reset request in a Packet_Out to reset the remote sensor state
+
+	; Initialize stack pointer, since no flow reaches before here, stack pointer is in INIT
+	ldi r16,low(NEWRAMEND)
+	out spl, r16
+	ldi r16,high(NEWRAMEND)
+	out sph, r16
+
 	;-----------
 	ldi r20, 0x00	; reset request
+    call CRC3
+	call TRANSMIT
+
+	push r20
+
+PRE_SERVICE_READOUT:
+	call SERVICE_READOUT
+	
+	; start == 1?
+	sbis pinf, 2	; if bit 2 in io register is cleard, continue
+	;no
+	rjmp INIT
+	;yes
+	ldi r16, 0x08
+	sts portf, r16
+
+	; receive == 1?
+	sbis pinf, 4	; if bit 4 in io register is set, continue
+	;no
+	rjmp PRE_SERVICE_READOUT
+	;yes
+	; receive == 0?
+POLL_RECV:
+	sbic pinf, 4	; if bit 4 in io register is cleard, continue
+	;no
+	rjmp POLL_RECV
+	;yes
+	sts portf, r1 ; portf = 0
+	in PACKET_IN_REGISTER, PIND
+
+	; Packet_in commmand type? (if bit 7 == 0, it is command)
+	sbrc PACKET_IN_REGISTER, 7
+	; no
+	rjmp TREAT_DATA ; first horizontal branch
+	; yes (command)
+
+	;tos has data packet?
+	; load tos to register
+	pop r16
+	in r16, spl
+	dec r16
+	out spl, r16 
+	sbrs r16, 7 ; if bit 7 == 1, it is a data packet (yes)
+	;no
+	rjmp CRC_CHECK3_PATH ; second horizontal branch
+	;yes
+	call CRC_CHECK11 ; expects return on r24
+	cpi r24, 0xFF
+	;true
+	breq LOG_REQUEST_PATH
+	;false (fail)
+	pop r16
+PRE_REPEAT_REQUEST:
+	call REPEAT_REQUEST
+	rjmp PRE_SERVICE_READOUT
+	;END
+
+TREAT_DATA: ; (HORIZONTAL BRANCH)
+	; stack empty? (if sp == ramend)
+	call IS_STACK_EMPTY
+	breq skip_TD
+	;no
+	pop r16
+skip_TD:
+	;yes
+	push PACKET_IN_REGISTER
+	rjmp PRE_SERVICE_READOUT
+
+CRC_CHECK3_PATH:
+	call CRC_CHECK3 ; expects return on r24
+	cpi r24, 0xFF
+	breq check3_pass
+	rjmp PRE_REPEAT_REQUEST
+
+	check3_pass:
+	; acknowledge in packet_in?
+	mov r16, PACKET_IN_REGISTER
+	andi r16, 0x60 ; mask it
+	cpi r16, 0x40 ;equal to 0100 0000 (acknowledge)?
+	;no
+	brne THIRD_H_BRANCH
+	;yes
+	;stack empty? (if stack == newramend)
+	call IS_STACK_EMPTY
+	;yes
+	breq PRE_SERVICE_READOUT
+	;no
+	pop r16
+	rjmp PRE_SERVICE_READOUT
+
+	rjmp PRE_SERVICE_READOUT
+
+THIRD_H_BRANCH:
+	;repeat?
+	mov r16, PACKET_IN_REGISTER
+	andi r16, 0x60
+	cpi r16, 0x60 ;repeat?
+	;no
+	brne PRE_SERVICE_READOUT
+	;yes
+	;stack emtpy?
+	call IS_STACK_EMPTY
+	;yes
+	breq PRE_SERVICE_READOUT
+	;no
+PRE_RETRANSMIT:
+	pop r20
+	call TRANSMIT
+	rjmp PRE_SERVICE_READOUT
+
+LOG_REQUEST_PATH:
+	mov r16, PACKET_IN_REGISTER
+	andi r16, 0x60
+	cpi r16, 0x20;log request?
+	;no
+	breq LOG
+	rjmp INIT
+	;yes
+LOG:
+	pop r16
+	st Y+, r16
+	call CHECK_MEM_POINTER
+	ldi r20, 0x40 ; TOS <- acknowledge
+	call CRC3
+	push r20
+	rjmp PRE_RETRANSMIT
+
+;---- custom subroutines ----
+CHECK_MEM_POINTER:
+	in r16, spl
+	in r17, sph
+	;if sp < y, don't reset y
+	cp r16, yl
+	cpc r17, yh
+	brsh skip
+	ldi yl, low(0x100)
+	ldi yh, high(0x100)
+	skip:
+	ret
+
+IS_STACK_EMPTY: ; sets the z using cp
+	ldi r16, LOW(RAMEND)
+	in r17, spl
+	cp r17, r16
+	ldi r16, HIGH(RAMEND)
+	in r17, sph
+	cpc r17, r16
+	ret
+
+WAIT_ONE_SECOND:
+	push r16
+	push r17
+	push r18
+
+	ldi r16, 60
+	loop1:
+		dec r16
+		ldi r17, 0xFF
+		loop2:
+			dec r17
+			ldi r18, 0xFF
+			;loop3: ; removed because simulation is slow
+				;dec r18
+				;cpse r18, r1
+				;rjmp loop3
+			cpse r17, r1
+			rjmp loop2
+		cpse r16, r1
+		rjmp loop1
+
+	pop r18
+	pop r17
+	pop r16
+	ret
+
+;---- subroutines specified in prompt ----
+TRANSMIT: ; transmits r20 to packet_out
+	out porte, r20
+	ret
+
+REPEAT_REQUEST:
+	push r20
+	ldi r20, 0b01100000
+	call CRC3
+	call TRANSMIT
+	pop r20
+	ret
+
+SERVICE_READOUT:
+	push r16
+
+	lds r16, pinf
+	sbrs r16, 0
+	rjmp start_mem_dump
+	
+	lds r16, pinf
+	sbrs r16, 1
+	rjmp transmit_last_entry
+
+	pop r16
+	ret
+
+	start_mem_dump:
+		push r20
+		ldi xl, low(0x100)
+		ldi xh, high(0x100)
+		;while x < y
+		mem_dump_loop:
+			cp xl, yl
+			cpc xh, yh
+			breq end
+			; do
+			ld r20, X+
+			out portb, r20
+			call WAIT_ONE_SECOND
+			rjmp mem_dump_loop
+
+	transmit_last_entry:
+		push r20
+		mov xl, yl
+		mov xh, yh
+		sbiw X, 1
+		ld r20, X
+		call TRANSMIT
+	end:
+		pop r20
+		pop r16
+		ret
 
 CRC3:
 	/* Modulo 2 example
@@ -91,61 +333,13 @@ CRC_CHECK3:
 	rcall CRC3 ; reads and writes to r20
 	cp packet, original_packet
 	breq CRC_CHECK3_pass	; if they are equal, store 0xFF (1, true) return value to r16
-	ldi r16, 0x00	; if not true, then store 0, false in r16
+	ldi r20, 0x00	; if not true, then store 0, false in r16
 	rjmp CRC_CHECK3_finish
 CRC_CHECK3_pass:
-	ldi r16, 0xFF	; store true
+	ldi r20, 0xFF	; store true
 CRC_CHECK3_finish:	
+	ret ; returns true or false in r20 (stand)
+
+CRC_CHECK11: ; (UNFINISHED)
+	ldi r24, 0x00
 	ret
-
-
-.org 0x900
-start:
-	; put your argument in r20
-	jmp CONF_PORTS	; will also do INIT since it is right beneath it
-	
-	ldi r20, 0x40 ; 0 00 00000 [command packet, 2-bit command, 5 itu code 
-    call CRC3
-	
-	//call SERVICE_READOUT
-	
-	sbis pinf,0	; bit 0 of pinf for START
-	; check if bit 0 is set, if START is asserted, do not go to INIT, continue
-	jmp INIT
-
-	// Ready <- ON
-	ldi r16, 0x00
-	sts ddrg, r16	; set bit 0 to Ready led
-	ldi r16,0xFF	
-	sts ping, r16 ; turn on the Ready led
-rec:
-	sbis pinf, 1	; bit 0 of pinf for Receive
-	rjmp rec	; loop until receive has been assserted
-
-	ldi r16, 0x00
-	sts ping, r16 ; turn off Ready
-
-	in r16, pind	; Packet_In
-	sbrs r16, 7		; if bit 7 in Packet_In is set, that means it's a Data Packet
-	jmp subr_c3_NO	; if it's Data Packet, then we go to "Yes" route in the diagram
-	; if not, then it's a Command Packet, and we go with the "No" route	
-branch1:
-	// <TOS has Data packet?>
-
-
-
-subr_c3_NO:
-	push r17
-	push r18
-	ldi r18, SPL
-	ldi r17, low(RAMEND)
-	cp r18, r17	; if stack pointer is at the bottom, stack is empty
-
-	breq subr_c3_NO_y
-	pop r18
-	pop r17
-subr_c3_NO_y:
-	// packet in is in r16, put it to top of stack
-	push r16
-
-
